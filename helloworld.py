1bcf0aed6307a3e87e71d1475681e615f276812f
Crystal D'Souza <crystalmariedsouza@Crystals-MacBook-Air.local>
2021-09-15 18:03:13 -0400

    Added an edit to the helloworld script

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,6 @@
 def main(): 
     print("Hello World, from Wasif! 4")
-
+    print("Hi Wasif! Thanks for completing the first ECE444 lab with me - excited to work with you more over the rest of the semester!")
 
 if __name__ == "__main__":
     main()
